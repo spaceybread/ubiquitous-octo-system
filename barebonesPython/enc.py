d04456fd1921bf0937cffc8de88a0273dac7697a
spaceybread <spaceybread@gmail.com>
2024-07-02 10:25:06 -0500

    Added Alice and shared computations

--- barebonesPython/enc.py
+++ barebonesPython/enc.py
@@ -24,18 +24,37 @@ bob_pem = bytes(bob_pem, 'utf-8')
 bob_public = load_pem_public_key(bob_pem, backend)
 bob_pub_bytes = bob_public.public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)[-65:]
 
-print(Fore.RED)
-print(type(bob_public))
-print(Fore.WHITE)
+#print(Fore.RED)
+#print(type(bob_public))
+#print(Fore.WHITE)
 
-print("Bob's public key (PEM format):")
-print("{}".format(bob_pem))
-print()
+#print("Bob's public key (PEM format):")
+#print("{}".format(bob_pem))
 
-print("Bob's public key bytes: {}".format(binascii.b2a_hex(bob_pub_bytes)))
+print(Fore.BLUE)
+print("Bob's public key in bytes: {}".format(binascii.b2a_hex(bob_pub_bytes)))
 
 # Alice enters the picture
 
 alice_priv = ec.generate_private_key(ec.SECP256R1(), backend)
 
 alice_pub_bytes = alice_priv.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)[-65:]
+
+print(Fore.GREEN)
+print("Alice's public key in bytes: {}".format(binascii.b2a_hex(alice_pub_bytes)))
+
+shared_key = alice_priv.exchange(ec.ECDH(), bob_public)
+
+print(Fore.RED)
+print("Shared key in bytes: {}".format(binascii.b2a_hex(shared_key)))
+
+xkdf = X963KDF(algorithm=hashes.SHA256(), length=16, sharedinfo=alice_pub_bytes, backend=backend)
+
+key_enc = xkdf.derive(shared_key)
+
+print(Fore.YELLOW)
+print("AES encryption key in bytes: {}".format(binascii.b2a_hex(key_enc)))
+
+IV = binascii.a2b_hex('00000000000000000000000000000000')
+
+
